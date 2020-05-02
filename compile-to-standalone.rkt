8f19fbd36a13343d38f69e22bd9a0e650310b706
Dexter Santucci <38226103+DexterLagan@users.noreply.github.com>
2020-05-02 19:28:42 +0200

    Added support for gui
    Added support for compiling GUI programs with GRacket.
    There's a bug with the icon file search. Still looking for a solution.

--- compile-to-standalone.rkt
+++ compile-to-standalone.rkt
@@ -14,11 +14,13 @@
 (define *app-name* "Compile Standalone")
 (define *raco-command-line* "raco exe ")
 (define *raco-embed-dlls* "--embed-dlls ")
+(define *raco-gui* "--gui ")
 (define *raco-icon-switch* "--ico ")
+(define *icon-dirname* "icon")
 
-(define *nix-icon-ext* ".png")
-(define *win-icon-ext* ".ico")
-(define *mac-icon-ext* ".icns")
+(define *nix-icon-ext* #".png")
+(define *win-icon-ext* #".ico")
+(define *mac-icon-ext* #".icns")
 
 ;;; defs
 
@@ -30,52 +32,70 @@
     (('macosx)  *mac-icon-ext*)))
 
 ;; find files of a given extension recursively from the current directory
-(define (find-files/ext ext)
-  (find-files (λ (f) (string-suffix? (path->string f) ext))))
+;Causes an error from Windows 10:
+;Error in script file "C:\\...\\compile-standalone.rkt":
+; path-extension=?: contract violation
+;  expected: (or/c bytes? string?)
+;  given: #<path:C:\Code\Racket\...\icon\plane_icon.ico>
+(define (find-files/ext path ext)
+  (define (ext-file? f)
+    (and (file-exists? f) ; regular files only
+         (path-has-extension? f ext)))
+  (find-files ext-file? path))
 
 ;; if an icon folder exists, returns the enclosing icon file path
-(define (get-icon-path icon-ext)
-  (if (directory-exists? "icon")
-      (let ((icon-files (find-files/ext icon-ext)))
+(define (get-icon-path parent-path icon-ext)
+  (define icon-dir
+    (build-path parent-path *icon-dirname*))
+  (if (directory-exists? icon-dir)
+      (let ((icon-files (find-files/ext icon-dir icon-ext)))
         (if (null? icon-files) #f
             (first icon-files)))
       #f))
 
 ;; returns the path to the folder containing f
-(define (get-parent-folder-str f)
+(define (get-parent-folder f)
   (define-values (base name folder?)
     (split-path f))
-  (path->string base))
+  base)
 
 ;; generates a raco command line, given the source file
-(define (get-raco-command-line os-type icon-path source-file)
+(define (get-raco-command-line os-type icon-path gui? source-file)
   (string-append *raco-command-line*
                  (if (eq? os-type 'windows) *raco-embed-dlls* "")
                  (if icon-path
                      (string-append *raco-icon-switch* icon-path " ") "")
+                 (if gui? *raco-gui* "")
                  (path->string source-file)))
 ; unit test
 (module+ test
-  (check-equal? (get-raco-command-line 'windows "icon/win-icon.ico" (string->path "source-file.rkt"))
-                "raco exe --embed-dlls --ico icon/win-icon.ico source-file.rkt")
-  (check-equal? (get-raco-command-line 'unix "icon/linux-icon.png" (string->path "source-file.rkt"))
+  (check-equal? (get-raco-command-line 'windows "icon/win-icon.ico" #t (string->path "source-file.rkt"))
+                "raco exe --embed-dlls --ico icon/win-icon.ico --gui source-file.rkt")
+  (check-equal? (get-raco-command-line 'unix "icon/linux-icon.png" #f (string->path "source-file.rkt"))
                 "raco exe --ico icon/linux-icon.png source-file.rkt")
-  (check-equal? (get-raco-command-line 'macosx #f (string->path "source-file-mac.rkt"))
-                "raco exe source-file-mac.rkt")
-  (check-equal? (get-raco-command-line 'macosx "icon/mac-icon.icns" (string->path "source-file-mac.rkt"))
+  (check-equal? (get-raco-command-line 'macosx #f #t (string->path "source-file-mac.rkt"))
+                "raco exe --gui source-file-mac.rkt")
+  (check-equal? (get-raco-command-line 'macosx "icon/mac-icon.icns" #f (string->path "source-file-mac.rkt"))
                 "raco exe --ico icon/mac-icon.icns source-file-mac.rkt"))
 
 ;;; main
 
-(define os-type (system-type))
-(define icon-ext (get-current-platforms-icon-ext os-type))
-(define icon-path (get-icon-path icon-ext))
-
 (define-script compile
   #:label "compile-standalone"
-  (λ (selection #:file f)
-    (if (system (get-raco-command-line os-type icon-path f))
-        (begin (shell-execute "explore" (get-parent-folder-str f) ""
+  (λ (selection #:file f #:definitions defs-text)
+
+    (define os-type (system-type))
+    (define icon-ext (get-current-platforms-icon-ext os-type))
+    (define parent-path (get-parent-folder f))
+    (define parent-path-str (path->string parent-path))
+    (define icon-path (get-icon-path parent-path icon-ext))
+   
+    (define gui?
+      (or (send defs-text find-string "#lang racket/gui")
+          (send defs-text find-string "(require racket/gui")))
+    (define raco-command (get-raco-command-line os-type icon-path gui? f))
+    (if (system raco-command)
+        (begin (shell-execute "explore" parent-path-str ""
                               (current-directory) 'sw_shownormal))
         (void (message-box *app-name* "Error during compilation.")))))
 
