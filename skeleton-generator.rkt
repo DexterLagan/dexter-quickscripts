3925ad76e9750b26b3de33eb3f3072e91dcb987e
Dexter Santucci <38226103+DexterLagan@users.noreply.github.com>
2020-05-17 19:18:32 +0200

    Added loading dialog

--- skeleton-generator.rkt
+++ skeleton-generator.rkt
@@ -33,6 +33,31 @@
 (define (confirm? question)
   (eq? 'yes (message-box *app-name* question #f (list 'yes-no))))
 
+;; a generic loading dialog
+(define loading-dialog
+  (let* ((new-dialog (new dialog%
+                          [label "Loading"]
+                          [parent #f]
+                          [width 225]
+                          [height 80]
+                          [min-width 225]
+                          [min-height 80]
+                          [stretchable-width #f]
+                          [stretchable-height #f]))
+         (new-panel (new vertical-panel% [parent new-dialog]
+                         [alignment '(center center)]))
+         (new-message (new message% [parent new-panel]
+                           [label "Please wait..."])))
+    new-dialog))
+
+;; helper function to show the loading dialog
+(define (show-loading)
+  (send loading-dialog show-without-yield))
+
+;; helper function to hide the loading dialog
+(define (hide-loading)
+  (send loading-dialog show #f))
+
 ;; displays an error and quits
 (define die
   (λ args
