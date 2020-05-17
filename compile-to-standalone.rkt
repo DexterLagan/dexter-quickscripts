45d4a3077c624a230717cac88dbbf3b21473c9d9
Dexter Santucci <38226103+DexterLagan@users.noreply.github.com>
2020-05-17 19:26:10 +0200

    Added loading box defs

--- compile-to-standalone.rkt
+++ compile-to-standalone.rkt
@@ -31,6 +31,31 @@
 
 ;;; defs
 
+;; a generic loading dialog
+(define loading-dialog
+  (let* ((new-dialog (new dialog%
+                          [label "Loading"]
+                          [parent #f]
+                          [width 225]
+                          [height 80]
+                          [min-width 225]
+                          [min-height 80]
+                          [stretchable-width #f]
+                          [stretchable-height #f]))
+         (new-panel (new vertical-panel% [parent new-dialog]
+                         [alignment '(center center)]))
+         (new-message (new message% [parent new-panel]
+                           [label "Please wait..."])))
+    new-dialog))
+
+;; helper function to show the loading dialog
+(define (show-loading)
+  (send loading-dialog show-without-yield))
+
+;; helper function to hide the loading dialog
+(define (hide-loading)
+  (send loading-dialog show #f))
+
 ;; get the current platform's icon extension
 (define (get-current-platforms-icon-ext os-type)
   (case os-type
